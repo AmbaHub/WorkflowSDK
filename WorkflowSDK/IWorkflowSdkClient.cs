2b77fa0a2c778c9dc72dd2aae7cfb7563aee0d8a
armands.garancs <armands.garancs@gmail.com>
2020-10-29 21:37:25 +0200

    high level task handeling.

--- WorkflowSDK/IWorkflowSdkClient.cs
+++ WorkflowSDK/IWorkflowSdkClient.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
 using WorkflowSDK.Core;
+using WorkflowSDK.Core.Model;
 using WorkflowSDK.Core.Model.DI;
+using WorkflowSDK.Core.Model.Workflow;
 using WorkflowSDK.Log;
 
 namespace WorkflowSDK
@@ -10,6 +13,10 @@ namespace WorkflowSDK
         IWorkflowManager WorkflowManager { get; }
         IStepFactory StepFactory { get; }
         ILogger Logger { get; }
+
+        Task Start<TData, TStep>(TData data, Action<IWorkflow> onCompletedWorkflow)
+            where TStep : Step
+            where TData : new();
 
     }
 }
--- WorkflowSDK/WorkflowSdkClient.cs
+++ WorkflowSDK/WorkflowSdkClient.cs
@@ -1,11 +1,18 @@
-﻿using WorkflowSDK.Core;
+﻿using System;
+using System.Threading.Tasks;
+using WorkflowSDK.Business;
+using WorkflowSDK.Core;
+using WorkflowSDK.Core.Model;
 using WorkflowSDK.Core.Model.DI;
+using WorkflowSDK.Core.Model.Workflow;
 using WorkflowSDK.Log;
 
 namespace WorkflowSDK
 {
     public class WorkflowSdkClient : IWorkflowSdkClient
     {
+        private readonly MainWorkflowManager _mainWorkflowManager;
+
         public IWorkflowManager WorkflowManager { get; }
         public IStepFactory StepFactory { get; }
         public ILogger Logger { get; }
@@ -15,11 +22,38 @@ namespace WorkflowSDK
             IStepFactory stepFactory,
             ILogger logger)
         {
+            _mainWorkflowManager = new MainWorkflowManager(stepFactory, workflowManager);
+
             WorkflowManager = workflowManager;
             StepFactory = stepFactory;
             Logger = logger;
         }
 
+        public async Task Start<TData, TStep>(TData data, Action<IWorkflow> onCompletedWorkflow) 
+            where TData : new() 
+            where TStep : Step
+        {
+            Logger.Log(LogLevel.Trace, "Main Workflow started.");
+            var task = _mainWorkflowManager.Start<TData, TStep>(data, onCompletedWorkflow);
+            await task;
+
+            if (task.IsCompletedSuccessfully)
+            {
+                Logger.Log(LogLevel.Trace, "Main Workflow completed successfully.");
+            }
+
+            if (task.Exception != null)
+            {
+                Logger.LogFatalException(task.Exception);
+            }
+
+            if (task.IsFaulted)
+            {
+                Logger.LogFatalException(FatalException.GetFatalException("Main workflow stoped unexpeted"));
+            }
+
+
+        }
         public void Dispose()
         {
             WorkflowManager?.Dispose();
